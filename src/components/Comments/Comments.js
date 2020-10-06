e24de889d7ec84ec86bcb983a166b08e85022355
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-10-06 12:10:55 -0700

    completed MVP

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -6,9 +6,14 @@ const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
+  const commentsList = comments.map(cur => {
+    return <Comment key={cur.id} comment={cur} /> 
+  })
+
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {commentsList}
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,9 +6,17 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+  console.log(posts); 
+
+  const postList = posts.map(cur => {
+    return <Post key={cur.id} post={cur} likePost={likePost} /> 
+  })
+
+
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {postList}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
